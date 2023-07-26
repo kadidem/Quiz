9293e41ff489c643d22685f63a8f98dd8c91c789
kadidia mah Dembele <kadidem1999@gmail.com>
2023-07-26 15:43:12 +0000

    first commit

---
+++ src/main/java/com/api/groupquiz/GroupquizApplication.java
@@ -0,0 +1,13 @@
+package com.api.groupquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupquizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupquizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/groupquiz/GroupquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.groupquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupquizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

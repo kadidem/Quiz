9808e1332667561f4db61b2ee5dd03c09b4ad66c
kadidia mah Dembele <kadidem1999@gmail.com>
2023-07-27 10:13:00 +0000

    kadi commit

---
+++ src/main/java/com/api/groupquiz/controleur/UtilisateurControleur.java
@@ -0,0 +1,40 @@
+package com.api.groupquiz.controleur;
+
+import com.api.groupquiz.entity.Utilisateur;
+import com.api.groupquiz.service.UtilisateurService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+@RestController
+@RequestMapping("/utilisateur")
+@AllArgsConstructor
+public class UtilisateurControleur {
+    private final UtilisateurService utilisateurService;
+    @PostMapping("/create")
+    public Utilisateur create (@RequestBody Utilisateur utilisateur){
+        return utilisateurService.create(utilisateur);
+    }
+    @GetMapping("/read")
+    public List<Utilisateur> read(){
+        return utilisateurService.read();
+    }
+    @PutMapping("/update/{id}")
+    public Utilisateur update(@PathVariable Long id, @RequestBody Utilisateur utilisateur){
+        return utilisateurService.update(id,utilisateur);
+    }
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+        return utilisateurService.delete(id);
+    }
+    @GetMapping("/read/{id}")
+    public Optional<Utilisateur> read1(@PathVariable Long id){
+        return utilisateurService.read1(id);
+    }
+
+}
---
+++ src/main/java/com/api/groupquiz/entity/Utilisateur.java
@@ -0,0 +1,24 @@
+package com.api.groupquiz.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String prenom_nom;
+    @Column(nullable = false,  unique = true)
+    private String password;
+}
